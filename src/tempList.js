5c2825f9b70acd0d14ee4e4e58e996bfff832fb5
Godwin Wabz <39406332+godwinwabwile@users.noreply.github.com>
2020-05-01 03:47:07 -0700

    Update tempList.js

--- src/tempList.js
+++ src/tempList.js
@@ -2,7 +2,7 @@ export const recipes = [
   {
     recipe_id: 35382,
     image_url:
-      "http://static.food2fork.com/Jalapeno2BPopper2BGrilled2BCheese2BSandwich2B12B500fd186186.jpg",
+      "https://unsplash.com/photos/ddZYOtZUnBk,
     title: "Jalapeno Popper Grilled Cheese Sandwich",
     publisher: "Closet Cooking",
     source_url:
@@ -11,7 +11,7 @@ export const recipes = [
   {
     recipe_id: 35383,
     image_url:
-      "http://static.food2fork.com/Jalapeno2BPopper2BGrilled2BCheese2BSandwich2B12B500fd186186.jpg",
+      "https://images.unsplash.com/photo-1473093226795-af9932fe5856?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=685&q=80",
     title: "Jalapeno Popper Grilled Cheese Sandwich",
     publisher: "Closet Cooking",
     source_url:
